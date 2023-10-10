011021a3879fddb367f289e73c0b47f04cc4f844
afzalpatwary <afzalhp2022@gmail.com>
2023-10-10 16:30:43 +0600

    banner running

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import logo from '../../assets/img/logo.svg'
+import logo from '/assets/img/logo.svg'
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
