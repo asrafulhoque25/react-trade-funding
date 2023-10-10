7f82009245f5763b60d8708c4cd1c4cdb758ceb2
afzalpatwary <afzalhp2022@gmail.com>
2023-10-10 18:39:13 +0600

    banner update

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -8,9 +8,9 @@ const Banner = () => {
   return (
     <section className='main-banner'>
       <div className="container m-auto">
-        <div className="grid grid-cols-2 gap-4 items-center pt-[170px]">
+        <div className="flex flex-col-reverse lg:grid lg:grid-cols-2 gap-4 items-center pt-[170px]">
           <div className='bannerContent'>
-            <h1 className='text-[56px] text-dark-dark-1 font-normal leading-[66px] mb-6'>
+            <h1 className='text-[36px] md:text-[46px] lg:text-[56px] text-dark-dark-1 font-normal leading-[1.25] mb-6'>
               Unlock cash flow & improve how your business transacts.
               </h1>
             <p className='text-dark-dark-2 text-[20px] font-bold mb-6'>
@@ -19,7 +19,7 @@ const Banner = () => {
               <CheckList  />
               <Button />
           </div>
-          <div className='bannerImage ms-auto'>
+          <div className='bannerImage pb-6 lg:pb-0 lg:ms-auto'>
             <img className='max-w-100 h-auto' src={BannerImg} alt="banner img" />
           </div>
         </div>
