fc8260a57c287aa7cfe2d49b873f18b588be666e
Md. Asraful Hoque <asraful15-6625@diu.edu.bd>
2023-10-11 10:57:56 +0600

    grow and save

--- src/components/growandsave/GrowSave.jsx
+++ src/components/growandsave/GrowSave.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-
+import backgroundImage from '/public/assets/img/growandsave.png';
 const GrowSave = () => {
     const styles = {
-        backgroundImage: 'url("/assets/img/grow-save.png")',
+        backgroundImage: `url(${backgroundImage})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       };
+    
+      
+const jsonData = {
+    key1: 'Bringing together the best products that fuel business <span style="color: #FF5E5A;"> Growth</span> and help you <span style="color: #FF5E5A;">Save</span>.',
+  };
   return (
     <div className='growandsave section-padding' style={styles}>
         <div className="container">
-            <h3>lasdkflasdflkasdf</h3>
+        <div className='common-title mx-auto' style={{maxWidth:"870px"}}>
+        <h2 className=' text-dark-dark-1 text-[48px]' dangerouslySetInnerHTML={{ __html: jsonData.key1 }} />
+        </div>
         </div>
     </div>
   )
