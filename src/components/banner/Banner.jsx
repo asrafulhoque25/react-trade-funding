172d3371173279ed8294c7104c9f409b375c9294
afzalpatwary <afzalhp2022@gmail.com>
2023-10-11 10:31:50 +0600

    banner  acheved done

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,10 +5,39 @@ import CheckList from './CheckList';
 import BannerImg from '/assets/img/home-banner-img.jpg';
 
 const Banner = () => {
+
+  const AchievedData = [
+    {
+      id: 1,
+      achieved: "98K+",
+      network: "Businesses network"
+    },
+    {
+      id: 2,
+      achieved: "5 star",
+      network: "Google rating"
+    },
+    {
+      id: 3,
+      achieved: "40+",
+      network: "Lenders and providers"
+    },
+    {
+      id: 4,
+      achieved: "160+ countries",
+      network: "Facilitating transactions across"
+    },
+    {
+      id: 5,
+      achieved: "USD 1.1 Billion",
+      network: "Loans delivered to SMEs"
+    },
+  ]
+
   return (
     <section className='main-banner'>
       <div className="container m-auto">
-        <div className="flex flex-col-reverse lg:grid lg:grid-cols-2 gap-4 items-center pt-[170px]">
+        <div className="flex flex-col-reverse lg:grid lg:grid-cols-2 gap-4 items-center pt-[170px] pb-[120px]">
           <div className='bannerContent'>
             <h1 className='text-[36px] md:text-[46px] lg:text-[56px] text-dark-dark-1 font-normal leading-[1.25] mb-6'>
               Unlock cash flow & improve how your business transacts.
@@ -24,11 +53,19 @@ const Banner = () => {
           </div>
         </div>
 
-        <div className="ourAchieved">
-          <div className="achieved">
-            <h5 className='text-dark-dark-1 text-[24px] font-normal'>98K+</h5>
-            <p className='text-dark-dark-3 text-[14px] font-normal'>Businesses network</p>
-          </div>
+        <div className="ourAchieved flex lg:justify-between flex-wrap gap-6 pb-10">
+          {
+            AchievedData.map((data, id) =>
+              <div key={id} className="achieved border-l-2 border-dark-dark-6 ps-4">
+                <h5 className='text-dark-dark-1 text-[24px] font-normal'>
+                  {data.achieved}
+                </h5>
+                <p className='text-dark-dark-3 text-[14px] font-normal'>
+                  {data.network}
+                </p>
+              </div>
+            )
+          }
         </div>
       </div>
     </section>
