f0fea1a6ee8fb0ae317f85aaeb7abef287cd8a60
Md. Asraful Hoque <asraful15-6625@diu.edu.bd>
2023-10-11 19:09:29 +0600

    desktop version done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Blogs from '../components/blogs/Blogs';
 import Brand from '../components/brand/Brand';
 import CommonFeature from '../components/commonFeature/CommonFeature';
 import CommonHeader from '../components/commonHeader/CommonHeader';
+import Footer from '../components/footer/Footer';
 import GrowSave from '../components/growandsave/GrowSave';
 import Testimonial from '../components/testimonial/Testimonial';
 const Home = () => {
@@ -29,6 +30,7 @@ const Home = () => {
         />
       } />
       <Brand />
+      <Footer />
     </>
     </div>
   )
